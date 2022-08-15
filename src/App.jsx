573bc93ebc4113229899c1931f225bf6f78a8d2f
Raphael Jay Bernardo <73016403+hackwebdev@users.noreply.github.com>
2022-08-15 13:57:31 +0800

    fetch data and useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { useEffect } from 'react'
 import './index.css'
 
-// adc9f17d
+const API_URL = 'http://www.omdbapi.com?apikey=adc9f17d'
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+
+    // console.log(data)
+    console.log(data.Search)
+  }
+
+  useEffect(() => {
+    searchMovies('Spiderman')
+  }, [])
+
   return (
     <>
       <h1>Hello React!!!</h1>
